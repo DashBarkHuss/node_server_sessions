e32b27f1c5d9c572819ce952484cb0c8c978c3e1
Dashiell BarkHuss <dashiellbarkhuss@Dashiells-MacBook-Pro.local>
2019-07-30 12:15:57 -0500

    request.data body removed

--- api.js
+++ api.js
@@ -1,29 +1,39 @@
+function action_user_login(){
+    console.log("login");
+}
 function respond(response, content){
     const jsontype = "{ 'Content-Type': 'application/json' }";
     content = JSON.stringify(content);
     response.writeHead(200, jsontype);
     response.end(content, 'utf-8');
 }
+function identify(a,b){
+    if (API.parts[1] === a && API.parts[2] === b) return true;
+    return false;
+}
 
 class API {
     constructor(){};
 
     static exec(request, response) {
-        request.chunks = [];
-        request.on('data', (segment)=>request.chunks.push(segment));
+        request.on('data', ()=>{});
         request.on('end', ()=>{
             console.log("API.parts: ", API.parts);
+
+            if (identify('user', 'login')){
+                action_user_login();
+            };
             respond(response, {success: true});
         });
-
-    }
+    };
     static catchAPIRequest(url){
-        console.log(url);
+        console.log("url:",url);
         if (url[0] === '/') url = url.substring(1, url.length);
         if (url.split('/')[0]='api'){
             API.parts = url.split('/');
             return true;
         }
+        return false;
     }
 }
 
--- index.js
+++ index.js
@@ -40,9 +40,6 @@ http.createServer((request, response)=>{
     });
 }).listen(port,ip);
 
-fetch('http://127.0.0.1:3000/api/lol').then(promise=>{
-    console.log("response: ", promise);
-    const json = promise.json();
-    console.log(".json(): ", json)
-    return json;
-});
+fetch('http://127.0.0.1:3000/api/user/login')
+.then(promise=>promise.json())
+.then(content=> console.log("content:",content));
